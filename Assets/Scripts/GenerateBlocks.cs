717959b1105b0f17669ad6d78780b2690d46db3a
winchell459 <johncmorris@gmail.com>
2021-01-12 16:10:12 -0800

    end of day 1/11/21

---
+++ Assets/Scripts/GenerateBlocks.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GenerateBlocks : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public TileBase tilebase;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UtilityTilemap.PlaceTiles(tilemap, new Vector3Int(0, 0, 0), 11, 1, tilebase);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UtilityTilemap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public static class UtilityTilemap
+{
+    public static void PlaceTile(Tilemap tilemap, Vector3Int pos, TileBase tileType)
+    {
+        tilemap.SetTile(pos, tileType);
+    }
+
+    public static void DestroyTile(Tilemap tilemap, Vector3Int pos)
+    {
+        tilemap.SetTile(pos, null);
+    }
+
+    public static void PlaceTiles(Tilemap tilemap, Vector3Int pos, float height, float width, TileBase tileType)
+    {
+        int xStart = pos.x - (int)(width / 2);
+        int yStart = pos.y - (int)(height / 2);
+        
+        for(int i = xStart; i < xStart + width; i += 1)
+        {
+            for(int j = yStart; j < yStart + height; j += 1)
+            {
+                PlaceTile(tilemap, new Vector3Int(i, j, pos.z), tileType);
+            }
+        }
+    }
+}
