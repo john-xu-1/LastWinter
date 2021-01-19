25e7aacda1e4e136b1534cb4c5910f627581586e
winchell459 <johncmorris@gmail.com>
2021-01-19 15:59:56 -0800

    end of day 1/18/21

---
+++ Assets/Scripts/Map/CollisionMap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CollisionMap : MonoBehaviour
+{
+    private List<CollisionTile> map = new List<CollisionTile>();
+    public TileBase Center, Right, Left, Up, Down, TopRight, BottomRight, TopLeft, BottomLeft;
+    public Tilemap Tilemap;
+    
+    public void AddCollsionTile(Vector2Int pos, int type)
+    {
+        CollisionTile tile = new CollisionTile(pos, type);
+        map.Add(tile);
+    }
+
+    public void DebugPlaceTiles()
+    {
+        foreach(CollisionTile tile in map)
+        {
+            if(tile.type > 0)
+            {
+                UtilityTilemap.PlaceTile(Tilemap, (Vector3Int)tile.pos, Center);
+                Debug.Log("Placing Tile");
+            }
+                
+        }
+    }
+
+}
+
+public class CollisionTile
+{
+    public Vector2Int pos;
+    public int type;
+
+    public CollisionTile(Vector2Int pos, int type)
+    {
+        this.pos = pos;
+        this.type = type;
+    }
+}
---
+++ Assets/Scripts/Map/DebugMap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMap : MonoBehaviour
+{
+    public MapGenerator Generator;
+
+    [TextArea(20,30)]
+    public string Bitmap;
+
+    private void Start()
+    {
+        if (Generator) Generator.ConvertMap(Bitmap);
+    }
+}
