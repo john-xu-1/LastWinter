fb6f3be21fdffc0016216e30806b3962e00fcd30
john-xu-1 <63273190+john-xu-1@users.noreply.github.com>
2021-02-01 18:09:21 -0800

    w

--- Assets/Scripts/CollisionMap.cs
+++ Assets/Scripts/CollisionMap.cs
@@ -6,7 +6,7 @@ using UnityEngine.Tilemaps;
 public class CollisionMap : MonoBehaviour
 {
     private List<CollisionTile> map = new List<CollisionTile>();
-    public TileBase Center, Right, Left, Up, Down, TopRight, BottomRight, TopLeft, BottomLeft;
+    public TileBase Center, Right, Left, Up, Down, TopRight, BottomRight, TopLeft, BottomLeft, LinkersTopRight, LinkersBottomRight, LinkersTopLeft, LinkersBottomLeft;
     public Tilemap tilemap;
 
     public void AddCollisionTiles(Vector2Int pos, int type)
@@ -14,6 +14,45 @@ public class CollisionMap : MonoBehaviour
         CollisionTile tile = new CollisionTile(pos, type);
         map.Add(tile);
     }
+    public void BuilMap()
+    {
+
+    }
+
+    public bool FindNeighbor(Vector2Int pos)
+    {
+        foreach (CollisionTile tile in map)
+        {
+            if (pos == tile.pos) return true;
+        }
+        return false;
+    }
+    public int[] FindNeighbors(Vector2Int startpos)
+    {
+        int[] neighbors = new int[9];
+        neighbors[4] = 1;
+        for (int i = -1; i < 2;  i += 1)
+        {
+            for (int j = -1; j < 2; j += 1)
+            {
+                Vector2Int pos = new Vector2Int(startpos.x + i, startpos.y + j);
+                int x = i + 1;
+                int y = -j + 1;
+                int index = x + 3 * y;
+                
+                if (pos != startpos && FindNeighbor(pos))
+                {
+                    neighbors[index] = 1;
+                }
+                else
+                {
+                    neighbors[index] = 0;
+                }
+            }
+        }
+        return neighbors;
+    }
+
 
     public void DebugPlaceTiles()
     {
@@ -21,7 +60,34 @@ public class CollisionMap : MonoBehaviour
         {
             if(tile.type == 1)
             {
-                UtilityTilemap.PlaceTile(tilemap, (Vector3Int)tile.pos, Center);
+                int[] neighbors = FindNeighbors(tile.pos);
+                if (neighbors[1]  == 1 && neighbors[3] == 1 && neighbors[5] == 1 && neighbors[7] ==1)
+                    UtilityTilemap.PlaceTile(tilemap, (Vector3Int)tile.pos, Center);
+                else if (neighbors[5] == 0)
+                    UtilityTilemap.PlaceTile(tilemap, (Vector3Int)tile.pos, Right);
+                else if (neighbors[3] == 0)
+                    UtilityTilemap.PlaceTile(tilemap, (Vector3Int)tile.pos, Left);
+                else if (neighbors[1] == 0)
+                    UtilityTilemap.PlaceTile(tilemap, (Vector3Int)tile.pos, Up);
+                else if (neighbors[7] == 0)
+                    UtilityTilemap.PlaceTile(tilemap, (Vector3Int)tile.pos, Down);
+                else if (neighbors[1] == 1 && neighbors[5] == 1)
+                    UtilityTilemap.PlaceTile(tilemap, (Vector3Int)tile.pos, TopRight);
+                else if (neighbors[3] == 1 && neighbors[1] == 1)
+                    UtilityTilemap.PlaceTile(tilemap, (Vector3Int)tile.pos, TopLeft);
+                else if (neighbors[7] == 1 && neighbors[5] == 1)
+                    UtilityTilemap.PlaceTile(tilemap, (Vector3Int)tile.pos, BottomRight);
+                else if (neighbors[7] == 1 && neighbors[3] == 1)
+                    UtilityTilemap.PlaceTile(tilemap, (Vector3Int)tile.pos, BottomLeft);
+                else if (neighbors[1] == 0 && neighbors[5] == 0)
+                    UtilityTilemap.PlaceTile(tilemap, (Vector3Int)tile.pos, LinkersTopRight);
+                else if (neighbors[3] == 0 && neighbors[1] == 0)
+                    UtilityTilemap.PlaceTile(tilemap, (Vector3Int)tile.pos, LinkersTopLeft);
+                else if (neighbors[7] == 0 && neighbors[5] == 0)
+                    UtilityTilemap.PlaceTile(tilemap, (Vector3Int)tile.pos, LinkersBottomRight);
+                else if (neighbors[7] == 0 && neighbors[3] == 0)
+                    UtilityTilemap.PlaceTile(tilemap, (Vector3Int)tile.pos, LinkersBottomLeft);
+
             }
             else if (tile.type == 2)
             {
--- Assets/Scripts/RandomBitmap.cs
+++ Assets/Scripts/RandomBitmap.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RandomBitmap : MonoBehaviour
 {
     public string myString;
-    string glyphs = "210";
+    string glyphs = "10";
     
     private void Awake()
     {
@@ -19,32 +19,7 @@ public class RandomBitmap : MonoBehaviour
                 myString += glyphs[Random.Range(0, glyphs.Length)];
 
             }
-
-            for (int k = 0; k < myString.Length; k++)
-            {
-
-                if (myString[k] == '2')
-                {
-                    Debug.Log("working1");
-                    if (myString[k + 1] <= myString[myString.Length - 1]) 
-                    {
-                        if (myString[k + 1] == '1')
-                        {
-                            Debug.Log("working2");
-                            myString.Remove(k + 1);
-                            myString.Insert(k + 1, "0");
-                        }
-
-                    }
-                    
-                }
-                else
-                {
-                    Debug.Log("not a 2");
-                }
-                //Debug.Log(myString);
-            }
-
+            
             myString += "@";
             myString = myString.Replace("@", System.Environment.NewLine);
             Debug.Log(myString[1]);
