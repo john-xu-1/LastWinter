bc1aac6b3949c08715a56aaa22ee1d33bfbb604f
john-xu-1 <63273190+john-xu-1@users.noreply.github.com>
2021-08-12 23:50:27 -0700

    8.12 cool map and cool lights and also new enemy

---
+++ Assets/Scripts/Enemy/EnemyBehaviorSpikes.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class EnemyBehaviorSpikes : EnemyBehaviorBase
+{
+    public bool isAttack = false;
+
+    public Tilemap tmap;
+
+    public TileBase tb;
+
+    public float yOffset = -1;
+
+    public int xIncrement = 1;
+
+    int dir;
+
+    List<Vector3Int> POSs = new List<Vector3Int>();
+
+    
+
+    public override void defaultAI()
+    {
+        Vector3 target = p.transform.position;
+        Vector2 direction = (target - transform.position).normalized;
+
+
+
+
+        if (direction.x > 0)
+        {
+            dir = 1;
+        }
+        else if (direction.x < 0)
+        {
+            dir = -1;
+        }
+
+
+        if (Vector2.Distance(transform.position, p.transform.position) <= angerRange)
+        {
+            isAttack = true;
+        }
+        else
+        {
+            isAttack = false;
+        }
+
+        if (isAttack)
+        {
+            
+            for (int i = 1; i < attackRange; i += xIncrement)
+            {
+                TileBase curTile = UtilityTilemap.GetTile(tmap, new Vector2(transform.  position.x + i, p.transform.position.y + yOffset));
+
+                if (curTile == null)
+                {
+                    Vector3Int pos = tmap.WorldToCell(new Vector2(transform.position.x + i * dir, p.transform.position.y + yOffset));
+                    UtilityTilemap.PlaceTile(tmap, pos, tb);
+
+                    POSs.Add(pos);
+
+                }
+                
+                
+
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(direction.x, rb.velocity.y);
+
+            if (POSs.Count > 0)
+            {
+                for (int i = 0; i < POSs.Count; i++)
+                {
+                    UtilityTilemap.DestroyTile(tmap, POSs[i]);
+                }
+
+                POSs.Clear();
+            }
+            
+
+            
+        }
+
+    }
+}
