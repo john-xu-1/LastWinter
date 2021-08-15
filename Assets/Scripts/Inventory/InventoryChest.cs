a982a2995b376f7516f8524448c47411c967fd74
john-xu-1 <63273190+john-xu-1@users.noreply.github.com>
2021-08-15 13:11:20 -0700

    8.15 chests!

---
+++ Assets/Scripts/Inventory/InventoryChest.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class InventoryChest : MonoBehaviour
+{
+    public Color32 chestColor;
+
+    public Color32[] colorLibrary = new Color32[4];
+
+    public string rarity;
+
+    public List<InventoryObjects> content;
+
+    public GameObject emptyPrefab;
+
+    public float objectEjectSpeed;
+
+    public Vector2 minMaxRandX;
+
+    public float interactRange = 2;
+
+    
+
+    private void Start()
+    {
+        if (rarity == "S")
+        {
+            chestColor = colorLibrary[0];
+        }
+        else if (rarity == "A")
+        {
+            chestColor = colorLibrary[1];
+        }
+        else if (rarity == "B")
+        {
+            chestColor = colorLibrary[2];
+        }
+        else if (rarity == "C")
+        {
+            chestColor = colorLibrary[3];
+        }
+
+        transform.GetChild(1).GetComponent<SpriteRenderer>().color = chestColor;
+        transform.GetChild(2).GetComponent<Light2D>().color = chestColor;
+    }
+
+    public void addItems(InventoryObjects[] items)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            content.Add(items[i]);
+        }
+    }
+
+    public void addItems(List<InventoryObjects> items)
+    {
+        for (int i = 0; i < items.Count; i++)
+        {
+            content.Add(items[i]);
+        }
+    }
+
+
+    public void onInteract()
+    {
+        for (int i = 0; i < content.Count; i++)
+        {
+
+            GameObject instance = Instantiate(emptyPrefab, transform.position, Quaternion.identity);
+
+            instance.GetComponent<Pickupable>().item = content[i];
+
+            instance.GetComponent<SpriteRenderer>().sprite = content[i].itemSprite;
+
+            Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
+
+            float randX = Random.Range(minMaxRandX.x, minMaxRandX.y);
+
+            rb.AddForce(new Vector2(randX, objectEjectSpeed), ForceMode2D.Impulse);
+
+            Debug.Log("doing" + i + " times");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (Vector2.Distance (transform.position, FindObjectOfType<PlatformerController>().transform.position) <= interactRange)
+            {
+                onInteract();
+            }
+        }
+    }
+
+}
