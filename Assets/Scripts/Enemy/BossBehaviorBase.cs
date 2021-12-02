71562b3f64d283a0d0395e064dfe8dcd342ab469
john-xu-1 <63273190+john-xu-1@users.noreply.github.com>
2021-12-01 16:31:32 -0800

    12.1

--- Assets/Scripts/Enemy/BossBehaviorBase.cs
+++ Assets/Scripts/Enemy/BossBehaviorBase.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossBehaviorBase : MonoBehaviour
+public class BossBehaviorBase : PhysicsObject
 {
     [SerializeField] protected LayerMask mask;
 
-    [SerializeField] protected Rigidbody2D rb;
+    //[SerializeField] protected Rigidbody2D rb;
 
     [SerializeField] protected float jumpSpeed = 5;
 
     private Vector2 direction;
+
+    [SerializeField] float xDistanceBuffer = 1;
 
     public enum AttackTypes
     {
@@ -31,59 +33,70 @@ public class BossBehaviorBase : MonoBehaviour
 
     [SerializeField] protected TraversingTypes traversingType;
 
-    private void FixedUpdate()
+    //private void FixedUpdate()
+    //{
+    //    handleMotion();
+    //}
+
+    protected override void ComputeVelocity()
     {
-        handleMotion();
+        targetVelocity = handleMotion();
+
+
+        velocity.y = targetVelocity.y;
     }
 
-    private void handleMotion()
+    private Vector2 handleMotion()
     {
         Vector2 movement = motion();
 
         float wallHeight = getWallHeight(direction.x);
 
-       if (wallHeight > 0 && wallHeight < 4)
+        if (wallHeight > 0 && wallHeight < 4)
         {
-            if (isGrounded()) movement = new Vector2(0, wallHeight * jumpSpeed);
+            if (isGround()) movement = new Vector2(0, wallHeight * jumpSpeed);
             else movement = new Vector2(0, movement.y);
         }
 
-        rb.velocity = movement;
+        //rb.velocity = movement;
+        return movement;
     }
 
     private float getWallHeight(float dir)
     {
         float rayDistance = 2;
-        for (int i = 3; i >= 0; i -= 1)
+        for (int i = 4; i >= 0; i -= 1)
         {
-            float hitDistance = PhysicsObject.raycast(transform.position + Vector3.up * (0.5f + i), dir * Vector3.right, rayDistance, mask);
+            float hitDistance = PhysicsObject.raycast(transform.position + Vector3.up * (0.01f + i), dir * Vector3.right, rayDistance, mask);
             if (hitDistance < rayDistance) return i + 1;
         }
-        
+
 
         return 0;
     }
 
-    private bool isGrounded()
+    private bool isGround()
     {
         for (int i = 0; i < 3; i += 1)
         {
-            float GroundDistance = PhysicsObject.raycast(transform.position + (-1 * i) *Vector3.right, Vector3.down, 0.1f, mask);
+            float GroundDistance = PhysicsObject.raycast(transform.position + (-1 + i) * Vector3.right, Vector3.down, 0.1f, mask);
             if (GroundDistance <= 0.1f) return true;
         }
-        
-        
+
+
         return false;
     }
 
     private Vector2 motion()
     {
-        Vector2 movement = rb.velocity;
+        Vector2 movement = velocity;
         if (traversingType == TraversingTypes.following)
         {
             Transform player = GameObject.FindGameObjectWithTag("Player").transform;
             direction = ((player.position - transform.position) * Vector2.right).normalized;
-            movement = new Vector2(direction.x * followSpeed(), rb.velocity.y);
+
+            if (Mathf.Abs(player.position.x - transform.position.x) < xDistanceBuffer) direction.x = 0;
+            movement = new Vector2(direction.x * followSpeed(), velocity.y);
 
         }
 
@@ -97,3 +110,4 @@ public class BossBehaviorBase : MonoBehaviour
     }
 
 }
+
