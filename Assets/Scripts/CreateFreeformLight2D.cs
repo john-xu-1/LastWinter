51b5866513c549b998a65f7743d14400c1f77c50
winchell459 <johncmorris@gmail.com>
2022-01-19 18:09:52 -0800

    end of class 1/19/22

---
+++ Assets/Scripts/CreateFreeformLight2D.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateFreeformLight2D : MonoBehaviour
+{
+    public UnityEngine.Experimental.Rendering.Universal.Light2D FreeformLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // FreeformLight.po
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
