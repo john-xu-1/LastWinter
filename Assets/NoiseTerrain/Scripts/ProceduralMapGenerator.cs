6a3b5a3d64006d957c3adc692a3cbedabbc7382c
winchell459 <johncmorris@gmail.com>
2023-01-09 18:05:34 -0800

    testing with roomSize

--- Assets/NoiseTerrain/Scripts/ProceduralMapGenerator.cs
+++ Assets/NoiseTerrain/Scripts/ProceduralMapGenerator.cs
@@ -22,6 +22,7 @@ namespace NoiseTerrain
         public List<Vector2Int> toFixChunkIDs = new List<Vector2Int>();
         public List<Vector2Int> toDisplayChunks = new List<Vector2Int>();
 
+        public Vector2Int roomSize = new Vector2Int(10, 10);
         public Transform target;
 
         
@@ -235,7 +236,11 @@ namespace NoiseTerrain
 
                             float threshold = 0;
                             
-                            if (toBuildChunks[i].x == tileRadius.x + chunkID.x || toBuildChunks[i].x == -tileRadius.x + chunkID.x || toBuildChunks[i].y == tileRadius.y + chunkID.y || toBuildChunks[i].y == -tileRadius.y + chunkID.y) threshold = -1;
+                            //if (toBuildChunks[i].x == tileRadius.x + chunkID.x || toBuildChunks[i].x == -tileRadius.x + chunkID.x 
+                            //    || toBuildChunks[i].y == tileRadius.y + chunkID.y || toBuildChunks[i].y == -tileRadius.y + chunkID.y)
+                            //    threshold = -1;
+                            if (toBuildChunks[i].x % roomSize.x == 0 ||  toBuildChunks[i].y % roomSize.y == 0)
+                                threshold = -1;
                             chunk = new Chunk(toBuildChunks[i], GenerateBoolMap(minX, maxX, minY, maxY, threshold), this);
                             chunks.Add(chunk);
                         }
